3a459987265c935513bf58ea96f925e7ab6d8496
zkw-student <58385356+zkw-student@users.noreply.github.com>
2022-09-19 10:17:34 +0800

    Toast修改

--- BroadCastRecv.java
+++ BroadCastRecv.java
@@ -1,5 +1,5 @@
 public class BroadCastRecv extends BroadCastReceiver{
     public void onReceive(Context context, Intent intent){
-      Toast.make(this,"这是第一个广播接受器",LENGTH_SHOW);
+      Toast.make(this,"这是第一个广播接受器",LENGTH_LONG);
     }
 }
